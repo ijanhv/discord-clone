65dc73300365c04d1d2fa5d17b5e9fa26c305b13
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-11-12 00:46:50 +0530

    add dark mode toggle

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,11 +1,16 @@
 
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
 
   return (
     <div className="flex">
-      This is a protected route
+      <UserButton 
+        afterSignOutUrl="/"
+      />
+      <ModeToggle />
     </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import {dark} from "@clerk/themes";
+
+import { ThemeProvider } from "@/components/ui/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -16,9 +20,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+    <ClerkProvider >
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#313338]"
+        )}>
+          <ThemeProvider 
+          
+          attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
+          {children}
+          </ThemeProvider>
+          </body>
       </html>
     </ClerkProvider>
   );
